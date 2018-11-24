ede1547b99fced56df84e8217ade822a45d72477
정연 김 <syjk506@naver.com>
2018-11-24 23:25:24 +0900

    초기 커밋.

---
+++ RandomSelect/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomSelect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+                    Console.WriteLine("몇개중 몇개를 뽑을 예정인가요오?");
+
+                    int all, select;
+                    all = int.Parse(Console.ReadLine());
+                    select = int.Parse(Console.ReadLine());
+
+                    for (int i = 0; i < select; i++)
+                    {
+                        Console.WriteLine("선택된 번호는 {0} 입니다아.", r.Next(1, all + 1));
+                    }
+
+                    Console.WriteLine("계속하려면 아무 키나 눌러주세요오");
+                    Console.ReadLine();
+           
+        }
+    }
+}
